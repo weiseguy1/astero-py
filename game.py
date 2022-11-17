15628de3088b29a3929205a62634ea8c43e01f18
Kyle Weise <kyle.a.weise@tutanota.com>
2022-11-17 12:26:08 -0600

    Change log:   - Added laser (in beta)   - Added border around score board

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
-import pygame
-import sys
+import pygame, sys
 
+# Game init
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -9,11 +9,15 @@ clock = pygame.time.Clock()
 
 bg_surf = pygame.image.load('graphics/background.png').convert()
 
-# Converts file to something pygame understands
+# ship import
 ship_surf = pygame.image.load('graphics/ship.png').convert_alpha()
 ship_rect = ship_surf.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
+# Laser
+laser_surf = pygame.image.load('graphics/laser.png').convert_alpha()
+laser_rect = laser_surf.get_rect(midbottom=(ship_rect.midtop))
 
+# Import text
 font = pygame.font.Font('graphics/subatomic.ttf', 50)
 text_surf = font.render('Space', True, 'White')
 text_rect = text_surf.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
@@ -28,11 +32,21 @@ while True:
     # Frame rate limit
     clock.tick(120)
 
+    # mouse input
+    ship_rect.center = pygame.mouse.get_pos()
+
+    # update
+    laser_rect.y -= 10
+
+    # Drawing
     display_surface.fill((0, 0, 0))
     display_surface.blit(bg_surf, (0, 0))
-    
-    
-    display_surface.blit(ship_surf, ship_rect)
-    display_surface.blit(text_surf, text_rect)
 
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, (255,255,255), text_rect.inflate(30, 30), width=8, border_radius=5)
+
+    display_surface.blit(laser_surf, laser_rect)
+    display_surface.blit(ship_surf, ship_rect)
+
+    # Drawing final frame
     pygame.display.update()
