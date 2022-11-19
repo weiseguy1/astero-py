01852f08afc51bec25d236a0fd93e911d5303ff2
Kyle Weise <kyle.a.weise@tutanota.com>
2022-11-18 18:33:21 -0600

    Change log:   - Added laser movement from mouse press   - Added score

--- game.py
+++ game.py
@@ -1,5 +1,18 @@
 import pygame, sys
 
+def laser_update(laser_list, speed = 300):
+    for rect in laser_list:
+        rect.y -= speed * dt
+        if rect.bottom < 0:
+            laser_list.remove(rect)
+
+def display_score():
+    score_text = f'Score: {pygame.time.get_ticks() // 1000}'
+    text_surf = font.render(score_text, True, 'White')
+    text_rect = text_surf.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, (255,255,255), text_rect.inflate(30, 30), width=8, border_radius=5)
+
 # Game init
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -15,12 +28,10 @@ ship_rect = ship_surf.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 # Laser
 laser_surf = pygame.image.load('graphics/laser.png').convert_alpha()
-laser_rect = laser_surf.get_rect(midbottom=(ship_rect.midtop))
+laser_list = []
 
 # Import text
 font = pygame.font.Font('graphics/subatomic.ttf', 50)
-text_surf = font.render('Space', True, 'White')
-text_rect = text_surf.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
 
 
 while True:
@@ -28,6 +39,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            laser_rect = laser_surf.get_rect(midbottom=ship_rect.midtop)
+            laser_list.append(laser_rect)
 
     # Frame rate limit
     dt = clock.tick(120) / 1000 # deltatime = px/frame / ms
@@ -35,17 +49,17 @@ while True:
     # mouse input
     ship_rect.center = pygame.mouse.get_pos()
 
-    # update
-    laser_rect.y -= round(200 * dt)
-
+    # Update position of laser
+    laser_update(laser_list)
+    
     # Drawing
     display_surface.fill((0, 0, 0))
     display_surface.blit(bg_surf, (0, 0))
 
-    display_surface.blit(text_surf, text_rect)
-    pygame.draw.rect(display_surface, (255,255,255), text_rect.inflate(30, 30), width=8, border_radius=5)
+    display_score()
 
-    display_surface.blit(laser_surf, laser_rect)
+    for rect in laser_list:
+        display_surface.blit(laser_surf, rect)
     display_surface.blit(ship_surf, ship_rect)
 
     # Drawing final frame
